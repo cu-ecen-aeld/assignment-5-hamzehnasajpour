c86739b6e9dba942d2d535ea5c389ea575725208
Hamzeh <hamzeh.nasajpour@gmail.com>
2023-10-02 10:15:44 +0100

    assignment3 hash commit updated

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6bc02b3b93dca6e2a4d45ff6c5ba0887f204976f'
+AESD_ASSIGNMENTS_VERSION = '07ea02020dd2fd6880cb2d3d7aa9923931d8db46'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
